4c0c639967e91736a5d04023994aaf9be8734afa
huynd-git <155427161+huynd-git@users.noreply.github.com>
2024-09-24 18:14:41 +0700

    Update extract_postgresql.py

--- Scripts/extract_postgresql.py
+++ Scripts/extract_postgresql.py
@@ -41,7 +41,7 @@ if conn is None:
 else:
     print("Connection established!")
 
-# extract data from sql server and save it in local machine as csv files
+# extract data from sql server and save it in the local machine as CSV files
 cursor = conn.cursor()
 file_path = path.dirname(__file__) + "\..\data"
 
@@ -53,15 +53,16 @@ for i in range(len(table_names)):
     query = "SELECT * FROM {}".format(table_names[i])
     local_filename = "{0}_{1}".format(table_names[i], suffix)
 
-    # execute sql query and get results 
+    # execute SQL query and get results 
     cursor.execute(query)
     results = cursor.fetchall()
 
-    # save table as csv files
+    # save table as CSV files
     with open(file_path + "\\" + local_filename, 'w') as f:
         csv_w = csv.writer(f, delimiter='|')
         csv_w.writerows(results)
 
+# close the connection
 cursor.close()
 conn.close()
 
@@ -76,12 +77,14 @@ access_key = parser.get("aws_s3_config", "access_key")
 secret_key = parser.get("aws_s3_config", "secret_key")
 bucket_name = parser.get("aws_s3_config", "bucket_name")
 
+# initiate s3 client
 s3 = boto3.client(
     's3',
     aws_access_key_id=access_key,
     aws_secret_access_key=secret_key
 )
 
+# set file name
 for i in range(len(table_names)):
     local_filename = "{0}_{1}".format(table_names[i], suffix)
     s3_filename = local_filename
